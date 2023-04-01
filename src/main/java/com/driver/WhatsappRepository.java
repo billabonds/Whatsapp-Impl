b83d654171072bdd0060a548436aa1c7afd50965
Shivam <goelshivam2012@gmail.com>
2023-04-01 22:32:10 +0530

    Done

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -23,7 +23,7 @@ public class WhatsappRepository {
         this.senderMap = new HashMap<Message, User>();
         this.adminMap = new HashMap<Group, User>();
         this.userMobile = new HashSet<>();
-        this.customGroupCount = 1;
+        this.customGroupCount = 0;
         this.messageId = 0;
     }
 
@@ -81,14 +81,15 @@ public class WhatsappRepository {
             setCustomGroupCount(2);
         }
         else {
+            customGroupCount++;
             String groupName = "Group " + customGroupCount;
             group.setName(groupName);
             group.setNumberOfParticipants(users.size());
-            customGroupCount++;
+            adminMap.put(group,users.get(0));
         }
 
         groupUserMap.put(group,users);
-        adminMap.put(group,users.get(0));
+
         return group;
     }
 
